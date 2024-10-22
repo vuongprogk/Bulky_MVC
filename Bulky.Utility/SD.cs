5ca5f5166c73b4c652430b4fcc9d9e76c4a4ead7
ace <vuong753333@gmail.com>
2024-10-22 19:02:04 +0700

    feat: add static details related to roles

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility
 {
+    // Static Details
     public static class SD
     {
+        public const String Role_Customer = "Customer";
+        public const String Role_Company = "Company";
+        public const String Role_Admin = "Admin";
+        public const String Role_Employee = "Employee";
     }
 }
