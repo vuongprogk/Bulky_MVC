011d384afa634bea4e778f3ec2fce97802b201af
ace <vuong753333@gmail.com>
2024-11-13 22:52:44 +0700

    feat: update static data

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -9,9 +9,22 @@ namespace BulkyBook.Utility
     // Static Details
     public static class SD
     {
-        public const String Role_Customer = "Customer";
-        public const String Role_Company = "Company";
-        public const String Role_Admin = "Admin";
-        public const String Role_Employee = "Employee";
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDeTayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusReliected = "Rejected";
+        
     }
 }
