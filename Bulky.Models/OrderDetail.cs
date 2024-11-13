f88948f3d4277cd981be33f5e8208b79dd7a0d22
ace <vuong753333@gmail.com>
2024-11-13 22:52:09 +0700

    feat: add models

--- Bulky.Models/OrderDetail.cs
+++ Bulky.Models/OrderDetail.cs
@@ -9,21 +9,24 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Models
 {
-    public class ShoppingCart
+    public class OrderDetail
     {
         public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
         public int ProductId { get; set; }
         [ForeignKey("ProductId")]
-        [ValidateNever]
+        [ValidateNever] 
         public Product Product { get; set; }
-        [Range(1, 1000, ErrorMessage ="Please enter value between 1 and 1000")]
-        public int Count { get; set; }
-        public string ApplicationUserId { get; set; }
-        [ForeignKey("ApplicationUserId")]
-        [ValidateNever]
-        public ApplicationUser ApplicationUser { get; set; }
 
-        [NotMapped]
+        public int Count { get; set; }
         public decimal Price { get; set; }
+
+
     }
 }
